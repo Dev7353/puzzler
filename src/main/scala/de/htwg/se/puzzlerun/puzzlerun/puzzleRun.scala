f20294600e6bf81422ba7b735bfd6c3e4337b1f0
Kirill Meng <kimeng@htwg-konstanz.de>
2016-11-04 11:08:31 +0100

    give user choice to continue

--- src/main/scala/de/htwg/se/puzzlerun/puzzlerun/puzzleRun.scala
+++ src/main/scala/de/htwg/se/puzzlerun/puzzlerun/puzzleRun.scala
@@ -24,7 +24,7 @@ object puzzleRun{
     loop.breakable {
       while(true) {
         tui.draw(controller.get_grid())
-        var eingabe = readLine("Eingabe: ").toCharArray
+        var eingabe = readLine("Eingabe: \n").toCharArray
 
         for (c <- eingabe) {
           c match {
@@ -32,23 +32,34 @@ object puzzleRun{
             case 'a' => controller.left()
             case 's' => controller.down()
             case 'd' => controller.right()
-            case _ => print("Falsche Eingabe.")
+            case _ => print("Falsche Eingabe.\n")
           }
           controller.state match {
-            case Some(false) => loop.break;
-            case Some(true) => tui.draw(controller.get_grid())
+            case 0 =>
+            case 1 => tui.draw(controller.get_grid())
+                                loop.break
+            case 2 => print("Du hast gewonnen!\n")
+                                loop.break
           }
 
           print("____________________________\n")
         }
     }
+    }
 
+    print("Weiterspielen?\n")
+    var eingabe = readLine("Eingabe: \n")
+
+    eingabe match {
+
+      case "j" => main(args = new Array[String](3))
+      case "n" => return
     }
   }
   def main(args: Array[String]): Unit = {
 
-    val grid = Grid(4, 4)
-    val player = Player(3, 3)
+    val grid = Grid(8, 8)
+    val player = Player(7, 7)
     val target = Target(0, 0)
     val obstacles:List[Obstacle] = List(Obstacle(1,1), Obstacle(2,1),Obstacle(2,0),Obstacle(3,2),Obstacle(1,3))
     val tui = new Tui()
