31f604c8436412cce84cbf3a01451633f6f6c9d3
Kirill Meng <kimeng@htwg-konstanz.de>
2016-12-02 09:32:44 +0100

    BUG-000 fix obstacle bug

--- src/main/scala/de/htwg/se/puzzlerun/view/Tui.scala
+++ src/main/scala/de/htwg/se/puzzlerun/view/Tui.scala
@@ -1,10 +1,10 @@
 package de.htwg.se.puzzlerun.view
 
-import com.sun.glass.ui.Timer
 import de.htwg.se.puzzlerun.controller._
 import de.htwg.se.puzzlerun.util._
 
 import scala.io.StdIn._
+import scala.util.control.Breaks
 
 case class Tui(var c: Controller) extends Observer {
   c.add(this)
@@ -20,25 +20,44 @@ case class Tui(var c: Controller) extends Observer {
   def input(): Int = {
     var eingabe = readLine("Eingabe: \n").toCharArray
     var eingabeLength = eingabe.length
-    for (c <- eingabe) {
-      c match {
-        case 'w' =>
-          this.c.up()
-          eingabeLength -= 1
-        case 'a' =>
-          this.c.left()
-          eingabeLength -= 1
-        case 's' =>
-          this.c.down()
-          eingabeLength -= 1
-        case 'd' =>
-          this.c.right()
-          eingabeLength -= 1
-        case _ =>
-          print("Falsche Eingabe. Wird ignoriert.\n")
-          eingabeLength -= 1
-      }
-    }
+    var loop = new Breaks
+
+   loop.breakable {
+     for (c <- eingabe) {
+       c match {
+         case 'w' =>
+           this.c.up()
+           eingabeLength -= 1
+         case 'a' =>
+           this.c.left()
+           eingabeLength -= 1
+         case 's' =>
+           this.c.down()
+           eingabeLength -= 1
+         case 'd' =>
+           this.c.right()
+           eingabeLength -= 1
+         case _ =>
+           print("Falsche Eingabe. Wird ignoriert.\n")
+           eingabeLength -= 1
+       }
+
+       this.c.state match {
+         case 0 =>
+           if (this.c.checkEingabeLength(eingabeLength)) {
+             loop.break
+           }
+         case 1 =>
+           loop.break
+         case 2 =>
+           loop.break
+         case 3 =>
+           loop.break
+       }
+     }
+   }
+
+
     eingabeLength
   }
   def defeat(reason: String): Unit = {
