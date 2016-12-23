d76f2512dbe2045e88d6c7c4a44ade02e0dd0258
Kirill Meng <kimeng@htwg-konstanz.de>
2016-12-23 09:13:51 +0100

    add first graphics

--- src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/Gui.scala
+++ src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/Gui.scala
@@ -2,6 +2,8 @@
   * Created by kmg on 16.12.16.
   */
 package de.htwg.se.puzzlerun.view.IGui.Impl1
+import javax.swing.{ImageIcon, JLabel}
+
 import de.htwg.se.puzzlerun.controller.IController
 import de.htwg.se.puzzlerun.util._
 import de.htwg.se.puzzlerun.view.IGui.IGui
@@ -11,8 +13,25 @@ import scala.swing._
 class Gui(var c: IController) extends MainFrame with Observer with IGui{
 
   title = "Puzzlrun"
-  preferredSize = new Dimension(320, 240)
-  contents = new Label("hier könnte Ihre Werbung stehen.")
+  preferredSize = new Dimension(640, 480)
+  resizable = false
+  contents = new GridPanel(c.grid.length+1, c.grid.height+1){
+
+    val player = new Label("", new ImageIcon(getClass.getResource("player.png")), Alignment.Right)
+
+    contents += player
+
+    for(i <- 1 to 16){
+      contents += new Label{
+        icon = new ImageIcon(getClass.getResource("gras.jpg"))
+      }
+
+    }
+
+
+
+  }
+
   visible = true
   def update = draw()
 
