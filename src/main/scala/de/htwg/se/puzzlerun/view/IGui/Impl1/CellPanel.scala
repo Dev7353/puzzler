45398ab24a4e49e060cb8750839ecb88ca35cd33
Kirill Meng <kimeng@htwg-konstanz.de>
2017-01-05 14:11:10 +0100

    add some primitive gui layout

---
+++ src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/CellPanel.scala
@@ -0,0 +1,20 @@
+/**
+  * Created by kmg on 23.12.16.
+  */
+
+package de.htwg.se.puzzlerun.view.IGui.Impl1
+
+import de.htwg.se.puzzlerun.controller.IController
+
+import scala.swing._
+
+class CellPanel(c: IController, x:Int, y:Int) extends FlowPanel{
+  var cell = c.grid.grid(x)(y)
+  var label = new Label(cell.toString)
+  def boxPanel = new BoxPanel(Orientation.Vertical){
+    preferredSize = new Dimension(10,10)
+    contents += label
+    border = Swing.BeveledBorder(Swing.Raised)
+  }
+}
+
--- src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/Gui.scala
+++ src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/Gui.scala
@@ -2,7 +2,6 @@
   * Created by kmg on 16.12.16.
   */
 package de.htwg.se.puzzlerun.view.IGui.Impl1
-import javax.swing.{ImageIcon, JLabel}
 
 import de.htwg.se.puzzlerun.controller.IController
 import de.htwg.se.puzzlerun.util._
@@ -15,23 +14,22 @@ class Gui(var c: IController) extends MainFrame with Observer with IGui{
   title = "Puzzlrun"
   preferredSize = new Dimension(640, 480)
   resizable = false
-  contents = new GridPanel(c.grid.length+1, c.grid.height+1){
+  def gridPanel = new GridPanel(c.grid.length+1, c.grid.height+1){
 
-    val player = new Label("", new ImageIcon(getClass.getResource("player.png")), Alignment.Right)
+    for{
+      i <- 0 until c.grid.grid.length
+      j <- 0 until c.grid.grid.length
+    }{
 
-    contents += player
-
-    for(i <- 1 to 16){
-      contents += new Label{
-        icon = new ImageIcon(getClass.getResource("gras.jpg"))
-      }
-
+      contents += new CellPanel(c, i, j)
     }
-
-
 
   }
 
+
+  contents = new BorderPanel{
+    add(gridPanel, BorderPanel.Position.Center)
+  }
   visible = true
   def update = draw()
 
