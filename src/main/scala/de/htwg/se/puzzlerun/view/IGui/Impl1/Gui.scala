2e505011975a00afe340c1c5302ba45924865e8b
Kirill Meng <kimeng@htwg-konstanz.de>
2017-01-18 16:25:43 +0100

    add border panel and door png

--- src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/Gui.scala
+++ src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/Gui.scala
@@ -10,132 +10,172 @@ import de.htwg.se.puzzlerun.controller.IController
 import de.htwg.se.puzzlerun.util._
 import de.htwg.se.puzzlerun.view.IGui.IGui
 
-import scala.swing._
+import scala.swing.{BoxPanel, _}
 import scala.swing.event.Key
 import scala.swing.event.KeyPressed
 
 class Gui(var c: IController) extends MainFrame with Observer with IGui {
 
-  c.add(this)
-  def update = draw()
+  var fields = Array.ofDim[BoxPanel](c.grid.height, c.grid.length)
+  val tf = new TextField(columns = 10)
 
-  title = "Puzzlrun"
+  var up = new Label("Up \n"+c.moves.get("Up").get)
+  var down = new Label("Down "+c.moves.get("Down").get)
+  var right = new Label("Right "+c.moves.get("Right").get)
+  var left = new Label("Left "+c.moves.get("Left").get)
 
-  //resizable = false
+  for{
+    row <- 0 until c.grid.length
+    col <- 0 until c.grid.height
 
-  contents = new BoxPanel(Orientation.Vertical) {
-
-    contents += new BoxPanel(Orientation.Horizontal) {
-
-      contents += new Label("LEVEL 0")
-      //serialisierung
-    }
-
-    contents += new GridPanel(c.grid.length, c.grid.height) {
-
-      for {
-
-        row <- 0 until c.grid.length
-        col <- 0 until c.grid.height
-      } {
-        contents += new BoxPanel(Orientation.Horizontal) {
-
-          contents += new BoxPanel(Orientation.Horizontal) {
-
-
-            if (c.grid.getCell(row, col).toString.equals("p")) {
-
-              contents += new Label {
-
-                icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/player.png")
-
-
-              }
-            }
-            else if (c.grid.getCell(row, col).toString.equals("o")) {
-              contents += new Label {
-
-                icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/obstacle.jpeg")
-
-              }
-            }
-            else {
-              contents += new Label {
-
-                icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/grass.png")
-
-              }
-            }
-          }
+  }{
+    fields(row)(col) = new BoxPanel(Orientation.Horizontal){
+      if (c.grid.getCell(row, col).toString.equals("p")) {
+        contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/player.png")
         }
-
       }
-    }
-
-    val tf = new TextField(columns = 20)
-    var loop = new Breaks()
-
-
-
-    contents += new BoxPanel(Orientation.Horizontal){
-
-      contents += new Label("Directions")
-      contents += tf
-      listenTo(tf.keys)
-
-
-      reactions += {
-        case KeyPressed(_,Key.Enter,_,_) => {
-          var eingabe = tf.text
-          var eingabeLength = eingabe.size
-
-          loop.breakable {
-            for (e <- eingabe) {
-              e match {
-                case 'w' =>
-                  c.up()
-                  eingabeLength -= 1
-                case 'a' =>
-                 c.left()
-                  eingabeLength -= 1
-                case 's' =>
-                  c.down()
-                  eingabeLength -= 1
-                case 'd' =>
-                  c.right()
-                  eingabeLength -= 1
-                case _ =>
-                  print("Falsche Eingabe. Wird ignoriert.\n")
-                  eingabeLength -= 1
-              }
-
-              c.state match {
-                case "Not reached" =>
-                  if (c.checkEingabeLength(eingabeLength)) {
-                    loop.break
-                  }
-                case "Obstacle reached" =>
-                  loop.break
-                case "Target reached" =>
-                  loop.break
-                case "Moves depleted" =>
-                  loop.break
-              }
-            }
-          }
-
-          tf.text = ""
+      else if(c.grid.getCell(row, col).toString.equals("X")){
+        contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/grass.png")
+        }
+      }
+      else if(c.grid.getCell(row, col).toString.equals("-")){
+        contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/grass.png")
+        }
+      }
+      else if(c.grid.getCell(row, col).toString.equals("o")){
+        contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/obstacle.jpeg")
         }
       }
     }
-
   }
 
-  resizable = false
+  c.add(this)
+  def update = draw()
+  title = "Puzzlrun"
+
+  def titleBox = new BoxPanel(Orientation.Horizontal) {
+
+    contents += new Label("LEVEL 0")
+    //serialisierung
+  }
+
+  def grid = new GridPanel(c.grid.length, c.grid.height) {
+
+    for {
+
+      row <- 0 until c.grid.length
+      col <- 0 until c.grid.height
+    } {
+      contents += fields(row)(col)
+    }
+  }
+
+  def directions = new FlowPanel{
+    contents += new Label("Directions")
+    contents += tf
+  }
+
+  def bla = new GridPanel(1, 4){
+
+    contents += up
+    contents += down
+    contents += right
+    contents += left
+  }
+  import BorderPanel.Position._
+  contents = new BorderPanel{
+    add(titleBox, North)
+    add(grid, Center)
+    add(directions, East)
+    add(bla, West)
+  }
+
+  listenTo(tf.keys)
+  for{
+    row <- 0 until c.grid.length
+    col <- 0 until c.grid.height
+  }{
+    listenTo(fields(row)(col))
+  }
+
+  listenTo(up)
+  listenTo(down)
+  listenTo(right)
+  listenTo(left)
+
+
+
+
+  reactions += {
+    case KeyPressed(_,Key.Enter,_,_) => {
+      var eingabe = tf.text
+      var eingabeLength = eingabe.size
+      print(eingabeLength + "\n")
+      for{
+        i <- 0 until eingabeLength
+      }{
+        if(eingabe.charAt(i).equals('w')){
+          c.up()
+        }
+        else if(eingabe.charAt(i).equals('s')){
+          c.down()
+        }
+        else if(eingabe.charAt(i).equals('d')){
+          c.right()
+        }
+        else if(eingabe.charAt(i).equals('a')){
+          c.left()
+        }
+
+      }
+    }
+  }
+  resizable = true
   visible = true
 
   def draw(): Unit = {
+    for {
+
+      row <- 0 until c.grid.length
+      col <- 0 until c.grid.height
+    } {
+      fields(row)(col).contents.clear()
+
+      if(c.grid.getCell(row, col).toString.equals("p")){
+        fields(row)(col).contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/player.png")
+        }
+      }
+      else if(c.grid.getCell(row, col).toString.equals("o")){
+        fields(row)(col).contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/obstacle.jpeg")
+        }
+      }
+      else if(c.grid.getCell(row, col).toString.equals("X")){
+        fields(row)(col).contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/grass.png")
+        }
+      }
+      else if(c.grid.getCell(row, col).toString.equals("-")){
+        fields(row)(col).contents += new Label(""){
+          icon = new ImageIcon("/Users/kmg/projects/puzzlerun/src/main/scala/de/htwg/se/puzzlerun/view/IGui/Impl1/grass.png")
+        }
+      }
 
 
+      fields(row)(col).repaint
+      fields(row)(col).revalidate()
+    }
+
+    up.text = "Up "+c.moves.get("Up").get
+    down.text = "Down "+c.moves.get("Down").get
+    right.text = "Right "+c.moves.get("Right").get
+    left.text = "Left "+c.moves.get("Left").get
+
+    this.repaint()
   }
 }
